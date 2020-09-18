452398352fba4e5313c7b7aea1bed0c6edbdaabf
Tyler Tremblay <tyler.tremblay@gmail.com>
2020-09-17 23:55:28 -0500

    h1

--- marketplace/amazon/src/App.js
+++ marketplace/amazon/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Hello Everybody, we doing it!
+      <h1>Hello Everybody, we doing it, finally!
       {/* Hearder */}
       {/* Home */}
       </h1>
