00014eaa866f591149fd6f86311f6f273c7ef0bc
Tyler Tremblay <tyler.tremblay@gmail.com>
2020-11-27 20:07:14 -0600

    Body text area

--- marketplace/amazon/src/Home.js
+++ marketplace/amazon/src/Home.js
@@ -2,9 +2,10 @@ import React from 'react';
 import "./Home.css";
 
 function Home() {
-    return
-    <div className="home">
+    return (<div className="home">
+        <p>This is Home</p>
     </div>
+    );
 }
 
 export default Home;
